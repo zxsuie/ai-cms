cb3b175bcccedd67e9f01776617ccdc8bcc53816
Mark Josh Sabeniano <markjosh112345@gmail.com>
2025-09-24 12:56:51 +0000

    there are still error when I clicked log visit. I'm assuming that the DB

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,6 @@
 
 import { createClient } from '@supabase/supabase-js';
-import type { StudentVisit, Medicine, RefillRequest, Appointment } from '@/lib/types';
+import type { StudentVisit, Medicine, Appointment } from '@/lib/types';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
